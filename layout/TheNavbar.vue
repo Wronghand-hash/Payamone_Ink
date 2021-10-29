63de34504daa21faa14c369b94506939a82247f8
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2021-10-29 13:22:34 +0330

    update

---
+++ layout/TheNavbar.vue
@@ -0,0 +1,86 @@
+<template>
+  <!-- <v-app> -->
+  <div
+    class="
+      fixed
+      w-screen
+      h-20
+      lg:h-20
+      bg-transparent
+      Navbar
+      opacity-100
+      z-50
+      flex
+      align-center
+      justify-start
+      lg:px-9
+    "
+  >
+    <div>
+      <button
+        class="menuButton p-2 text-white text-xl font-bold scale-125"
+        @click="tog()"
+      >
+        &#9776;
+      </button>
+    </div>
+    <nav
+      :class="open ? 'navbar-open' : 'navbar-close'"
+      class="
+        navbar
+        w-64
+        absolute
+        overflow-x-scroll
+        bg-gray-700
+        top-0
+        left-0
+        h-screen
+      "
+    >
+      <div class="flex pr-2 justify-end">
+        <button class="p-2 text-white text-xl font-bold" @click="tog()">
+          &#9747;
+        </button>
+      </div>
+      <h1 class="text-xl text-center font-bold pt-5">Menu</h1>
+      <ul class="list-none text-white text-center">
+        <li class="my-8"><a href="#"></a>Teams</li>
+        <li class="my-8"><a href="#"></a>Projects</li>
+        <li class="my-8"><a href="#"></a>Favourites</li>
+        <li class="my-8"><a href="#"></a>Notifications</li>
+        <li class="my-8"><a href="#"></a>Members</li>
+      </ul>
+    </nav>
+  </div>
+  <!-- </v-app> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+    }
+  },
+  methods: {
+    tog() {
+      this.open = !this.open
+    },
+    menuColorBlack() {
+      const gsap = this.$gsap
+
+      gsap.to('.menuButton', {
+        color: 'black',
+      })
+    },
+     menuColorWhite() {
+    const gsap = this.$gsap
+
+    gsap.to('.menuButton', {
+      color: 'white',
+    })
+  },
+  },
+ 
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div>
+      <Navbar ref="TheNavbar" />
+    </div>
+    <div class="firstContainer bg-black h-screen w-screen">
+      <div
+        class="
+          w-full
+          h-full
+          flex
+          items-start
+          flex-col
+          justify-center
+          align-center
+        "
+      >
+        <div
+          class="text-5xl text-white p-5"
+          @mouseover="colorChange"
+          @mouseleave="colorReset"
+        >
+          <h1 class="titles">Your Skin is Your Canvas</h1>
+        </div>
+        <div ref="blackBackground" class="text-white text-6xl p-8">
+          <h1 class="titles">Only You have , your tattoos</h1>
+        </div>
+      </div>
+
+
+
+      <div>
+          <ImageContiner /> 
+      </div>
+
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../layout/TheNavbar.vue'
+import ImageContiner from '~/components/imageContainer.vue'
+
+export default {
+  name: 'App',
+  components: { Navbar , ImageContiner },
+  methods: {
+    colorChange() {
+      const gsap = this.$gsap
+
+      gsap.to('.firstContainer', {
+        backgroundColor: 'white',
+        ease: 'power2.out',
+      })
+      gsap.to('.titles', {
+        color: 'black',
+        ease: 'power2.out',
+      })
+
+      this.$refs.TheNavbar.menuColorBlack()
+    },
+    colorReset() {
+      const gsap = this.$gsap
+
+      gsap.to('.firstContainer', {
+        backgroundColor: 'black',
+        ease: 'power2.out',
+      })
+        gsap.to('.titles', {
+        color: 'white',
+        ease: 'power2.out',
+      })
+       this.$refs.TheNavbar.menuColorWhite()
+    },
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Miltonian+Tattoo&display=swap');
+
+
+.navbar {
+  transition: all 330ms ease-out;
+}
+
+.navbar-open {
+  transform: translateX(0%);
+}
+.navbar-close {
+  transform: translateX(-100%);
+}
+
+.titles{
+  font-family: 'Miltonian Tattoo', cursive;
+}
+</style>
